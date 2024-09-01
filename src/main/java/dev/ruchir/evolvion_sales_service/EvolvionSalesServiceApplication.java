0b04d094d49c431818a775e86297784ecc37e43e
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-01 23:47:11 +0530

    Initial commit with base project setup

---
+++ src/main/java/dev/ruchir/evolvion_sales_service/EvolvionSalesServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_sales_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvionSalesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolvionSalesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruchir/evolvion_sales_service/EvolvionSalesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_sales_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolvionSalesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
