74f57e38d05caff7ae87b72bb245756a3245d348
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-02 22:16:25 +0530

    Added ErrorResponse Class

---
+++ src/main/java/dev/ruchir/evolvion_sales_service/controller_advise/ErrorResponse.java
@@ -0,0 +1,19 @@
+package dev.ruchir.evolvion_sales_service.controller_advise;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private String errorCode;
+    private LocalDateTime timestamp;
+    private HttpStatus status;
+    private String errorId; // Optional field for tracking errors
+}
