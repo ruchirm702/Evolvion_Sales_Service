992c58fb89564838eefef3080e9fa8c8551fd894
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-05 12:45:41 +0530

    Added OrderConroller

---
+++ src/main/java/dev/ruchir/evolvion_sales_service/controller/OrderController.java
@@ -0,0 +1,52 @@
+package dev.ruchir.evolvion_sales_service.controller;
+
+import dev.ruchir.evolvion_sales_service.dto.OrderDTO;
+import dev.ruchir.evolvion_sales_service.service.interfaces.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        OrderDTO createdOrder = orderService.createOrder(orderDTO);
+        return ResponseEntity.ok(createdOrder);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long id, @RequestBody OrderDTO orderDTO) {
+        OrderDTO updatedOrder = orderService.updateOrder(id, orderDTO);
+        return ResponseEntity.ok(updatedOrder);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<OrderDTO>> getOrderById(@PathVariable Long id) {
+        Optional<OrderDTO> orderDTO = orderService.getOrderById(id);
+        return ResponseEntity.ok(orderDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> getAllOrders() {
+        List<OrderDTO> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+}
