77f5eaa476f4069128d4c1722b59785aba198c48
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-07 00:11:32 +0530

    Updated Order Controller

--- src/main/java/dev/ruchir/evolvion_sales_service/controller/OrderController.java
+++ src/main/java/dev/ruchir/evolvion_sales_service/controller/OrderController.java
@@ -4,13 +4,15 @@ import dev.ruchir.evolvion_sales_service.dto.OrderDTO;
 import dev.ruchir.evolvion_sales_service.service.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/orders")
+@Validated
 public class OrderController {
 
     private final OrderService orderService;
@@ -21,27 +23,27 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+    public ResponseEntity<OrderDTO> createOrder(@Valid @RequestBody OrderDTO orderDTO) {
         OrderDTO createdOrder = orderService.createOrder(orderDTO);
         return ResponseEntity.ok(createdOrder);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long id, @RequestBody OrderDTO orderDTO) {
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long id, @Valid @RequestBody OrderDTO orderDTO) {
         OrderDTO updatedOrder = orderService.updateOrder(id, orderDTO);
         return ResponseEntity.ok(updatedOrder);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<OrderDTO>> getOrderById(@PathVariable Long id) {
-        Optional<OrderDTO> orderDTO = orderService.getOrderById(id);
-        return ResponseEntity.ok(orderDTO);
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping
     public ResponseEntity<List<OrderDTO>> getAllOrders() {
-        List<OrderDTO> orders = orderService.getAllOrders();
-        return ResponseEntity.ok(orders);
+        return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     @DeleteMapping("/{id}")
